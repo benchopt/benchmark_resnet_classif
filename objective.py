8195c70fd7c4eecb8832b8d5524d6008ffe68611
zaccharieramzi <email@example.com>
2022-03-21 16:37:26 +0100

    rewrote objective to have customized outputs of functions

--- objective.py
+++ objective.py
@@ -1,7 +1,13 @@
 from benchopt import BaseObjective
+from pytorch_lightning import LightningModule, Trainer
+from torch import Tensor
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, TensorDataset
+import torchvision.models as models
 
 
 class Objective(BaseObjective):
+    """Classification objective"""
     name = "ResNet classification fitting"
 
     # XXX: this might be a good spot to specify the size of the ResNet
@@ -9,17 +15,57 @@ class Objective(BaseObjective):
     }
 
 
-    def __init__(self,):
-        pass
+    def __init__(self, _debug=False):
+        self.model = models.resnet18()
+        self._debug = _debug
+        self.trainer = Trainer()
 
     def set_data(self, X, y):
         self.X = X
         self.y = y
+        if self._debug:
+            self.X = self.X[:10]
+            self.y = self.y[:10]
+        self.dataset = TensorDataset(Tensor(X), Tensor(y))
+        self.data_loader = DataLoader(self.dataset, batch_size=64)
+        self.pl_module = BenchPLModule(self.model, self.data_loader)
 
-    def compute(self, beta):
-        # TODO: change
-        diff = self.y - self.X.dot(beta)
-        return .5 * diff.dot(diff)
+    def compute(self, pl_module):
+        loss = self.trainer.test(pl_module)
+        return loss
 
     def to_dict(self):
-        return dict(X=self.X, y=self.y)
+        return dict(
+            pl_module=self.pl_module,
+            trainer=self.trainer,
+        )
+
+
+class BenchPLModule(LightningModule):
+    """Lightning module for benchopt"""
+    def __init__(self, model, loader):
+
+        super().__init__()
+        self.model = model
+        self.loader = loader
+
+
+    def forward(self, x):
+        x = self.model(x)
+        return F.log_softmax(x, dim=1)
+
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        return self.test_step(batch, batch_idx)
+
+    def train_dataloader(self):
+        return super().train_dataloader()
+
+    def test_dataloader(self):
+        return self.test_loader
