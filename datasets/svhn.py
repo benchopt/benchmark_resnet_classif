6c9415ac81d287016bf432814b8906205123fcfa
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-08 15:11:54 +0200

    added possibility to svhn to return tf dataset

--- datasets/svhn.py
+++ datasets/svhn.py
@@ -1,22 +1,37 @@
-from benchopt import BaseDataset, safe_import_context
+from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
+    import tensorflow as tf
+    import tensorflow_datasets as tfds
     from torchvision import transforms
     import torchvision.datasets as datasets
+    import numpy as np
+
+    MultiFrameworkDataset = import_ctx.import_from(
+        'multi_frameworks_dataset',
+        'MultiFrameworkDataset',
+    )
 
 
-class Dataset(BaseDataset):
+class Dataset(MultiFrameworkDataset):
 
     name = "SVHN"
 
-    def get_data(self):
+    # from
+    # https://deepobs.readthedocs.io/en/v1.2.0-beta0_a/_modules/deepobs/pytorch/datasets/svhn.html
+    normalization_mean = (0.4376821, 0.4437697, 0.47280442)
+    normalization_std = (0.19803012, 0.20101562, 0.19703614)
+
+    parameters = {
+        'framework': ['pytorch', 'tensorflow'],
+    }
+
+    def get_torch_data(self):
         transform = transforms.Compose([
             transforms.ToTensor(),
-            # from
-            # https://deepobs.readthedocs.io/en/v1.2.0-beta0_a/_modules/deepobs/pytorch/datasets/svhn.html
             transforms.Normalize(
-                (0.4376821, 0.4437697, 0.47280442),
-                (0.19803012, 0.20101562, 0.19703614),
+                self.normalization_mean,
+                self.normalization_std,
             ),
         ])
         svhn_trainset = datasets.SVHN(
@@ -27,3 +42,15 @@ class Dataset(BaseDataset):
         )
 
         return 'object', dict(dataset=svhn_trainset)
+
+    def get_tf_data(self):
+        ds = tfds.load('svhn_cropped', split='train',  as_supervised=True)
+        normalization_layer = tf.keras.layers.Normalization(
+            mean=self.normalization_mean,
+            variance=np.square(self.normalization_std),
+        )
+        ds = ds.map(
+            lambda x, y: (normalization_layer(x), y),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        )
+        return 'dataset', dict(dataset=ds)
