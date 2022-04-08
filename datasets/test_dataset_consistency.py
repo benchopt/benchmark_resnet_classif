e766e17dee76b255403e71dc572b6ce0466524da
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-08 18:54:10 +0200

    refactored dataset consistency test

--- datasets/test_dataset_consistency.py
+++ datasets/test_dataset_consistency.py
@@ -22,18 +22,27 @@ def torch_dataset_to_np_array(torch_dataset, n_samples):
     return X, y
 
 
-def test_simulated_consistency():
-    from datasets import simulated
-    d_tf = simulated.Dataset.get_instance(framework='tensorflow')
-    d_torch = simulated.Dataset.get_instance(framework='pytorch')
-    _, tf_data = d_tf.get_data()
-    tf_dataset = tf_data['dataset']
-    _, torch_data = d_torch.get_data()
-    torch_dataset = torch_data['dataset']
-    tf_np_array = tf_dataset_to_np_array(tf_dataset, d_tf.n_samples)
-    torch_np_array = torch_dataset_to_np_array(
-        torch_dataset,
-        d_torch.n_samples,
+def test_datasets_consistency():
+    from datasets import (
+        cifar,
+        mnist,
+        simulated,
+        svhn,
     )
-    np.testing.assert_array_equal(tf_np_array[0], torch_np_array[0])
-    np.testing.assert_array_equal(tf_np_array[1], torch_np_array[1])
+    # datasets_list = [cifar, mnist, simulated, svhn]
+    datasets_list = [simulated]
+    for dataset in datasets_list:
+        d_tf = simulated.Dataset.get_instance(framework='tensorflow')
+        d_torch = simulated.Dataset.get_instance(framework='pytorch')
+        _, tf_data = d_tf.get_data()
+        _, torch_data = d_torch.get_data()
+        for dataset_type in ['dataset', 'test_dataset']:
+            tf_dataset = tf_data[dataset_type]
+            torch_dataset = torch_data[dataset_type]
+            tf_np_array = tf_dataset_to_np_array(tf_dataset, d_tf.n_samples)
+            torch_np_array = torch_dataset_to_np_array(
+                torch_dataset,
+                d_torch.n_samples,
+            )
+            np.testing.assert_array_equal(tf_np_array[0], torch_np_array[0])
+            np.testing.assert_array_equal(tf_np_array[1], torch_np_array[1])
