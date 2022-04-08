2d26dd5a1f7c648bbf17c452b9ec8c80925d1165
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-08 19:27:13 +0200

    corrected n samples and channel position in test dataset consistency

--- datasets/test_dataset_consistency.py
+++ datasets/test_dataset_consistency.py
@@ -23,6 +23,12 @@ def torch_dataset_to_np_array(torch_dataset, n_samples):
     return X, y
 
 
+def assert_tf_images_equal_torch_images(tf_images, torch_images):
+    # make torch_images channel last
+    torch_images = np.transpose(torch_images, (0, 2, 3, 1))
+    np.testing.assert_array_equal(tf_images, torch_images)
+
+
 @pytest.mark.parametrize('dataset_module_name', [
     'cifar',
     'mnist',
@@ -41,13 +47,16 @@ def test_datasets_consistency(dataset_module_name):
     d_torch = dataset.Dataset.get_instance(framework='pytorch')
     _, tf_data = d_tf.get_data()
     _, torch_data = d_torch.get_data()
-    for dataset_type in ['dataset', 'test_dataset']:
+    for dataset_type, n_samples_key in zip(
+        ['dataset', 'test_dataset'],
+        ['n_samples_train', 'n_samples_test'],
+    ):
+        n_samples = tf_data[n_samples_key]
+        assert n_samples == torch_data[n_samples_key], \
+            'Number of samples is different'
         tf_dataset = tf_data[dataset_type]
         torch_dataset = torch_data[dataset_type]
-        tf_np_array = tf_dataset_to_np_array(tf_dataset, d_tf.n_samples)
-        torch_np_array = torch_dataset_to_np_array(
-            torch_dataset,
-            d_torch.n_samples,
-        )
-        np.testing.assert_array_equal(tf_np_array[0], torch_np_array[0])
+        tf_np_array = tf_dataset_to_np_array(tf_dataset, n_samples)
+        torch_np_array = torch_dataset_to_np_array(torch_dataset, n_samples)
+        assert_tf_images_equal_torch_images(tf_np_array[0], torch_np_array[0])
         np.testing.assert_array_equal(tf_np_array[1], torch_np_array[1])
