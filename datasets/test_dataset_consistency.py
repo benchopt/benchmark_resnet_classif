e85dfda976419e758d17992a16f920b9567cb033
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-08 18:57:19 +0200

    made the dataset consistency test parametrized to have a better view of the failures

--- datasets/test_dataset_consistency.py
+++ datasets/test_dataset_consistency.py
@@ -1,5 +1,6 @@
 from benchopt.utils.safe_import import set_benchmark
 import numpy as np
+import pytest
 from torch.utils.data import DataLoader
 
 # this means this test has to be run from the root
@@ -22,27 +23,37 @@ def torch_dataset_to_np_array(torch_dataset, n_samples):
     return X, y
 
 
-def test_datasets_consistency():
+@pytest.mark.parametrize('dataset_name', [
+    'cifar',
+    'mnist',
+    'simulated',
+    'svhn',
+])
+def test_datasets_consistency(dataset_name):
     from datasets import (
         cifar,
         mnist,
         simulated,
         svhn,
     )
-    # datasets_list = [cifar, mnist, simulated, svhn]
-    datasets_list = [simulated]
-    for dataset in datasets_list:
-        d_tf = simulated.Dataset.get_instance(framework='tensorflow')
-        d_torch = simulated.Dataset.get_instance(framework='pytorch')
-        _, tf_data = d_tf.get_data()
-        _, torch_data = d_torch.get_data()
-        for dataset_type in ['dataset', 'test_dataset']:
-            tf_dataset = tf_data[dataset_type]
-            torch_dataset = torch_data[dataset_type]
-            tf_np_array = tf_dataset_to_np_array(tf_dataset, d_tf.n_samples)
-            torch_np_array = torch_dataset_to_np_array(
-                torch_dataset,
-                d_torch.n_samples,
-            )
-            np.testing.assert_array_equal(tf_np_array[0], torch_np_array[0])
-            np.testing.assert_array_equal(tf_np_array[1], torch_np_array[1])
+    dataset_mapping = {
+        'cifar': cifar,
+        'mnist': mnist,
+        'simulated': simulated,
+        'svhn': svhn,
+    }
+    dataset = dataset_mapping[dataset_name]
+    d_tf = dataset.Dataset.get_instance(framework='tensorflow')
+    d_torch = dataset.Dataset.get_instance(framework='pytorch')
+    _, tf_data = d_tf.get_data()
+    _, torch_data = d_torch.get_data()
+    for dataset_type in ['dataset', 'test_dataset']:
+        tf_dataset = tf_data[dataset_type]
+        torch_dataset = torch_data[dataset_type]
+        tf_np_array = tf_dataset_to_np_array(tf_dataset, d_tf.n_samples)
+        torch_np_array = torch_dataset_to_np_array(
+            torch_dataset,
+            d_torch.n_samples,
+        )
+        np.testing.assert_array_equal(tf_np_array[0], torch_np_array[0])
+        np.testing.assert_array_equal(tf_np_array[1], torch_np_array[1])
