e36744b7cfb329b80b86e12c3d549fe6e9434e8a
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-08 19:00:35 +0200

    added framework in data spec

--- utils/multi_frameworks_dataset.py
+++ utils/multi_frameworks_dataset.py
@@ -26,7 +26,7 @@ class MultiFrameworkDataset(BaseDataset, ABC):
         )
 
     def get_torch_data(self):
-        data_dict = dict(**self.ds_description)
+        data_dict = dict(framework=self.framework, **self.ds_description)
         for key, train in zip(['dataset', 'test_dataset'], [True, False]):
             data_dict[key] = self.torch_ds_klass(
                 root='./data',
@@ -37,7 +37,7 @@ class MultiFrameworkDataset(BaseDataset, ABC):
         return 'object', data_dict
 
     def get_tf_data(self):
-        data_dict = dict(**self.ds_description)
+        data_dict = dict(framework=self.framework, **self.ds_description)
         for key, split in zip(['dataset', 'test_dataset'], ['train', 'test']):
             ds = tfds.load(
                 self.tf_ds_name,
