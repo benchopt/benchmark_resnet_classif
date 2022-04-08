7fd0ae12dfd3a7363d0b8add7dfec4d2e894a086
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-08 18:43:26 +0200

    boosted possibilities of multi framework dataset abc

--- utils/multi_frameworks_dataset.py
+++ utils/multi_frameworks_dataset.py
@@ -1,20 +1,55 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
-from benchopt import BaseDataset
+from benchopt import BaseDataset, safe_import_context
+
+with safe_import_context() as import_ctx:
+    import numpy as np
+    import tensorflow as tf
+    import tensorflow_datasets as tfds
+    from torchvision import transforms
 
 
 class MultiFrameworkDataset(BaseDataset, ABC):
     def __init__(self, framework='pytorch'):
         # Store the parameters of the dataset
         self.framework = framework
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+                self.normalization_mean,
+                self.normalization_std,
+            ),
+        ])
+        self.image_preprocessing = tf.keras.layers.Normalization(
+            mean=self.normalization_mean,
+            variance=np.square(self.normalization_std),
+        )
 
-    @abstractmethod
     def get_torch_data(self):
-        ...
+        data_dict = dict(**self.ds_description)
+        for key, train in zip(['dataset', 'test_dataset'], [True, False]):
+            data_dict[key] = self.torch_ds_klass(
+                root='./data',
+                train=train,
+                download=True,
+                transform=self.transform,
+            )
+        return 'object', data_dict
 
-    @abstractmethod
     def get_tf_data(self):
-        ...
+        data_dict = dict(**self.ds_description)
+        for key, split in zip(['dataset', 'test_dataset'], ['train', 'test']):
+            ds = tfds.load(
+                self.tf_ds_name,
+                split=split,
+                as_supervised=True,
+            )
+            ds = ds.map(
+                lambda x, y: (self.image_preprocessing(x), y),
+                num_parallel_calls=tf.data.experimental.AUTOTUNE,
+            )
+            data_dict[key] = ds
+        return 'object', data_dict
 
     def get_data(self):
         if self.framework == 'pytorch':
