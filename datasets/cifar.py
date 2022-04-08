5115c3def676a58f1768ebbe879abbe840d5773b
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-08 15:14:22 +0200

    added possibility to cifar to return tf dataset

--- datasets/cifar.py
+++ datasets/cifar.py
@@ -1,22 +1,37 @@
-from benchopt import BaseDataset, safe_import_context
+from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
+    import tensorflow as tf
+    import tensorflow_datasets as tfds
     from torchvision import transforms
     import torchvision.datasets as datasets
+    import numpy as np
+
+    MultiFrameworkDataset = import_ctx.import_from(
+        'multi_frameworks_dataset',
+        'MultiFrameworkDataset',
+    )
 
 
-class Dataset(BaseDataset):
+class Dataset(MultiFrameworkDataset):
 
     name = "CIFAR"
 
-    def get_data(self):
+    # from
+    # https://github.com/kuangliu/pytorch-cifar/blob/master/main.py#L34
+    normalization_mean = (0.4914, 0.4822, 0.4465)
+    normalization_std = (0.2023, 0.1994, 0.2010)
+
+    parameters = {
+        'framework': ['pytorch', 'tensorflow'],
+    }
+
+    def get_torch_data(self):
         transform = transforms.Compose([
             transforms.ToTensor(),
-            # from
-            # https://github.com/kuangliu/pytorch-cifar/blob/master/main.py#L34
             transforms.Normalize(
-                (0.4914, 0.4822, 0.4465),
-                (0.2023, 0.1994, 0.2010),
+                self.normalization_mean,
+                self.normalization_std,
             ),
         ])
         cifar_trainset = datasets.CIFAR10(
@@ -29,3 +44,15 @@ class Dataset(BaseDataset):
         data = dict(dataset=cifar_trainset)
 
         return 'object', data
+
+    def get_tf_data(self):
+        ds = tfds.load('cifar', split='train',  as_supervised=True)
+        normalization_layer = tf.keras.layers.Normalization(
+            mean=self.normalization_mean,
+            variance=np.square(self.normalization_std),
+        )
+        ds = ds.map(
+            lambda x, y: (normalization_layer(x), y),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE,
+        )
+        return 'dataset', dict(dataset=ds)
