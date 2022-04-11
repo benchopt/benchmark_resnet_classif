35c833a00eb710829e904c819a02c09ff0948744
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-11 15:14:31 +0200

    implemented a custom strategy that allows the model to be kept on gpu after testing

--- utils/torch_helper.py
+++ utils/torch_helper.py
@@ -7,6 +7,14 @@ with safe_import_context() as import_ctx:
     from torchmetrics import Accuracy
     from pytorch_lightning import LightningModule
     from pytorch_lightning.callbacks import Callback
+    from pytorch_lightning.strategies import (
+        DDPStrategy,
+        DistributedDataParallel,
+        log,
+        rank_zero_info,
+        _TORCH_GREATER_EQUAL_1_11,
+        TrainerFn,
+    )
 
 
 # Convert benchopt benchmark into a lightning callback, used to monitor the
@@ -49,3 +57,39 @@ class BenchPLModule(LightningModule):
 
     def training_step(self, batch, batch_idx):
         return self.loss_logits_y(batch)[0]
+
+
+class DDPStrategyNoTeardown(DDPStrategy):
+    # from
+    # https://pytorch-lightning.readthedocs.io/en/1.6.0/_modules/pytorch_lightning/strategies/ddp.html#DDPStrategy.teardown
+    def teardown(self):
+        log.detail(f"{self.__class__.__name__}: tearing down strategy")
+        super().teardown()
+
+        if isinstance(self.model, DistributedDataParallel):
+            if (
+                _TORCH_GREATER_EQUAL_1_11
+                and not self.model.static_graph
+                and self.model._get_ddp_logging_data().get("can_set_static_graph")
+            ):
+                rank_zero_info(
+                    "Your model can run with static graph optimizations. For future training runs, we suggest you"
+                    f" pass `Trainer(..., strategy={self.__class__.__name__}(static_graph=True))` to enable them."
+                )
+            # unwrap model
+            self.model = self.lightning_module
+
+        if (
+            self.lightning_module.trainer is not None
+            and self.lightning_module.trainer.state.fn == TrainerFn.FITTING
+            and self._layer_sync
+        ):
+            # `self.lightning_module.trainer` can be None if teardown gets called on an exception before
+            # the trainer gets set on the LightningModule
+            self.model = self._layer_sync.revert(self.model)
+
+        if self.root_device.type == "cuda":
+            # GPU teardown
+            log.detail(f"{self.__class__.__name__}: !not! moving model to CPU")
+            # clean up memory
+            torch.cuda.empty_cache()
