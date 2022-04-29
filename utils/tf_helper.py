1a1e812106a892009036f39baa81a962101c8112
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-30 01:00:11 +0200

    corrected numpy import tf helper

--- utils/tf_helper.py
+++ utils/tf_helper.py
@@ -2,6 +2,7 @@ from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
 
+    import numpy as np
     import tensorflow as tf
     from keras.utils import io_utils
     from tensorflow.keras import backend
