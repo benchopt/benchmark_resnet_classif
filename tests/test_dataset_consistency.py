a0d4398db8773dd929485d53066a6d1997777708
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-29 20:37:58 +0200

    trying with random projection to fix test dataset consistency

--- tests/test_dataset_consistency.py
+++ tests/test_dataset_consistency.py
@@ -3,6 +3,7 @@ import warnings
 
 import numpy as np
 import pytest
+from sklearn import random_projection
 from sklearn.neighbors import NearestNeighbors
 from torch.utils.data import DataLoader
 
@@ -94,22 +95,26 @@ def test_datasets_consistency(dataset_module_name, dataset_type):
     try:
         assert_tf_images_equal_torch_images(X_tf, X_torch)
     except AssertionError:
-        clf = NearestNeighbors(n_neighbors=1)
+        transformer = random_projection.GaussianRandomProjection(n_components=4)
+        X_tf_proj = transformer.fit_transform(X_tf.reshape(X_tf.shape[0], -1))
         X_torch_channel_last = np.transpose(X_torch, (0, 2, 3, 1))
-        clf.fit(X_tf.reshape(X_tf.shape[0], -1))
-        indices = clf.kneighbors(
+        X_torch_proj = transformer.transform(
             X_torch_channel_last.reshape(X_torch_channel_last.shape[0], -1),
+        )
+        clf = NearestNeighbors(n_neighbors=1)
+        clf.fit(X_tf_proj)
+        indices = clf.kneighbors(
+            X_torch_proj,
             return_distance=False,
         )
-        ordered_X_torch = X_torch[indices]
+        ordered_X_torch = X_torch[indices[:, 0]]
         assert_tf_images_equal_torch_images(X_tf, ordered_X_torch)
-        y_torch = y_torch[indices]
-    finally:
-        if dataset_module_name != 'svhn' or dataset_type == 'test_dataset':
-            np.testing.assert_array_equal(y_tf, y_torch)
-        else:
-            warnings.warn(
-                'Label equality test not carried for SVHN '
-                'because of a weird duplicate with 2 different '
-                'labels',
-            )
+        y_torch = y_torch[indices[:, 0]]
+    if dataset_module_name != 'svhn' or dataset_type == 'test_dataset':
+        np.testing.assert_array_equal(y_tf, y_torch)
+    else:
+        warnings.warn(
+            'Label equality test not carried for SVHN '
+            'because of a weird duplicate with 2 different '
+            'labels',
+        )
