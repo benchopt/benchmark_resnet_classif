2e110b40e4b154cb87eca7305ecfc722b01ca409
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-29 10:54:43 +0200

    removed the initialize model weights fn

--- utils/torch_helper.py
+++ utils/torch_helper.py
@@ -32,23 +32,6 @@ class BenchPLModule(LightningModule):
         self.model = model
         self.accuracy = Accuracy()
 
-    def initialize_model_weights(self):
-        for m in self.model.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(
-                    m.weight,
-                    mode="fan_out",
-                    nonlinearity="relu",
-                )
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.constant_(m.bias, 0)
-
     def forward(self, x):
         x = self.model(x)
         return F.log_softmax(x, dim=1)
