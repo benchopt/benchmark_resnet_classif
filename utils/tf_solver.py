3226b57e3ec7859e9723424440c4ca4810704887
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-06 15:35:38 +0200

    corrected tf solver application of weight decay for sgd

--- utils/tf_solver.py
+++ utils/tf_solver.py
@@ -43,9 +43,9 @@ class TFSolver(BaseSolver):
         # NOTE: in the following, we need to multiply by the weight decay
         # by the learning rate to have a comparable setting with PyTorch
         self.coupled_wd = getattr(self, 'coupled_weight_decay', 0.0)
-        if self.coupled_wd == 0.0:
-            self.coupled_wd = getattr(self, 'weight_decay', 0.0)
         self.decoupled_wd = getattr(self, 'decoupled_weight_decay', 0.0)
+        if self.decoupled_wd == 0.0:
+            self.decoupled_wd = getattr(self, 'weight_decay', 0.0)
         if self.lr_schedule == 'step':
             self.lr_scheduler, self.wd_scheduler = [
                 tf.keras.optimizers.schedules.PiecewiseConstantDecay(
