e783218d2a30ffc15e8cda59c3a81db4f9e52f67
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-13 11:59:47 +0200

    added switch on horizontal flip in torch solver

--- utils/torch_solver.py
+++ utils/torch_solver.py
@@ -51,12 +51,15 @@ class TorchSolver(BaseSolver):
         self.model_init_fn = model_init_fn
         self.normalization = normalization
         self.framework = framework
+        self.symmetry = symmetry
 
         if self.data_aug:
-            data_aug_transform = transforms.Compose([
+            data_aug_list = [
                 transforms.RandomCrop(32, padding=4),
-                transforms.RandomHorizontalFlip(),
-            ])
+            ]
+            if self.symmetry is not None and 'horizontal' in self.symmetry:
+                data_aug_list.append(transforms.RandomHorizontalFlip())
+            data_aug_transform = transforms.Compose(data_aug_list)
         else:
             data_aug_transform = None
         self.dataset = AugmentedDataset(
