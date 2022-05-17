c6ec741b4eb096d71edc5f2d4c33656337b28ae1
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-17 18:41:04 +0200

    added a function to change the classification head of vgg for tf

---
+++ utils/tf_vgg.py
@@ -0,0 +1,28 @@
+from benchopt import safe_import_context
+
+with safe_import_context() as import_ctx:
+    from keras import layers
+    from keras import models
+
+
+def change_classification_head(large_model):
+    """"Removes the big dense layers at the end of the large model
+    and replaces them with a smaller one.
+    """
+    trimmed_model = models.Model(
+        inputs=large_model.inputs,
+        outputs=large_model.get_layer('flatten').outputs
+    )
+    classes = large_model.output_shape[-1]
+    small_dense = layers.Dense(512, activation='relu', name='fc1')
+    classification_dense = layers.Dense(
+        classes,
+        activation='softmax',
+        name='predictions',
+    )
+    small_model = models.Sequential([
+        trimmed_model,
+        small_dense,
+        classification_dense,
+    ])
+    return small_model
