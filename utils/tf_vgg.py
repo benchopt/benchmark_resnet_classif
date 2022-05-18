baf2cfe99882e3c0927e0ab0ec84040c6e8123fa
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-18 14:44:34 +0200

    corrected init of classification head in tf vgg

--- utils/tf_vgg.py
+++ utils/tf_vgg.py
@@ -1,6 +1,7 @@
 from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
+    from keras import initializers
     from keras import layers
     from keras import models
 
@@ -9,17 +10,30 @@ def change_classification_head(large_model):
     """"Removes the big dense layers at the end of the large model
     and replaces them with a smaller one.
     """
+    torch_init = initializers.VarianceScaling(
+        scale=1.0,
+        mode='fan_in',
+        distribution='uniform',
+    )
     trimmed_model = models.Model(
         inputs=large_model.inputs,
         outputs=large_model.get_layer('flatten').output,
     )
     classes = large_model.output_shape[-1]
-    small_dense = layers.Dense(512, activation='relu', name='fc1')
+    small_dense = layers.Dense(
+        512,
+        activation='relu',
+        name='fc1',
+        kernel_initializer=torch_init,
+        bias_initializer=torch_init,
+    )
     dropout = layers.Dropout(0.5, name='dropout')
     classification_dense = layers.Dense(
         classes,
         activation='softmax',
         name='predictions',
+        kernel_initializer=torch_init,
+        bias_initializer=torch_init,
     )
     small_model = models.Sequential([
         trimmed_model,
