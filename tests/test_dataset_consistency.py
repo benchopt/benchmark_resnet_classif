29941d34cc001f542dd5ecde4289dfccab3e3d27
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-06-08 11:57:20 +0200

    corrected import name for cifar 100 in test

--- tests/test_dataset_consistency.py
+++ tests/test_dataset_consistency.py
@@ -62,7 +62,7 @@ class AugmentedDataset(Dataset):
 
 @pytest.mark.parametrize('dataset_module_name', [
     'cifar',
-    'cifar100',
+    'cifar_100',
     'mnist',
     'simulated',
     'svhn',
@@ -77,7 +77,7 @@ def test_datasets_consistency(dataset_module_name, dataset_type):
         pytest.skip('SVHN dataset is too heavy for CI')
     from datasets import (  # noqa: F401
         cifar,
-        cifar100,
+        cifar_100,
         mnist,
         simulated,
         svhn,
