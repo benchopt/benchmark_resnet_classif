4ae8e14789e9f823052ae0fc1352ae54b50a511e
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-06-30 19:54:45 +0200

    removed wide resnets from torch resnets

--- utils/torch_resnets.py
+++ utils/torch_resnets.py
@@ -2,8 +2,6 @@ from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
     import torch
-    from torchvision.models import ResNet
-
 
 def remove_initial_downsample(large_model):
     large_model.conv1 = torch.nn.Conv2d(
@@ -16,16 +14,3 @@ def remove_initial_downsample(large_model):
     )
     large_model.maxpool = torch.nn.Identity()
     return large_model
-
-
-def wide_resnet(n_layers, widening_factor):
-    layers_map = {
-        16: [1, ]
-
-    }
-    model = ResNet(
-        ResNet.BasicBlock,
-        [n_layers, n_layers, n_layers],
-
-    )
-    return model
