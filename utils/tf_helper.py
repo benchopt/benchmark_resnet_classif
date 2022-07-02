08d1fec3311d3ea02f09b17949c0900f0829d8e7
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-07-02 23:04:51 +0200

    added a random resized crop util for tf

--- utils/tf_helper.py
+++ utils/tf_helper.py
@@ -67,6 +67,65 @@ with safe_import_context() as import_ctx:
                     self.model.optimizer.weight_decay,
                 )
 
+    class RandomResizedCrop(tf.keras.layers.Layer):
+        # taken from
+        # https://keras.io/examples/vision/nnclr/#random-resized-crops
+        def __init__(self, scale, ratio):
+            super(RandomResizedCrop, self).__init__()
+            self.scale = scale
+            self.log_ratio = (tf.math.log(ratio[0]), tf.math.log(ratio[1]))
+
+        def call(self, images):
+            batch_size = tf.shape(images)[0]
+            height = tf.shape(images)[1]
+            width = tf.shape(images)[2]
+
+            random_scales = tf.random.uniform(
+                (batch_size,),
+                self.scale[0],
+                self.scale[1]
+            )
+            random_ratios = tf.exp(tf.random.uniform(
+                (batch_size,),
+                self.log_ratio[0],
+                self.log_ratio[1]
+            ))
+
+            new_heights = tf.clip_by_value(
+                tf.sqrt(random_scales / random_ratios),
+                0,
+                1,
+            )
+            new_widths = tf.clip_by_value(
+                tf.sqrt(random_scales * random_ratios),
+                0,
+                1,
+            )
+            height_offsets = tf.random.uniform(
+                (batch_size,),
+                0,
+                1 - new_heights,
+            )
+            width_offsets = tf.random.uniform(
+                (batch_size,),
+                0,
+                1 - new_widths,
+            )
+
+            bounding_boxes = tf.stack(
+                [
+                    height_offsets,
+                    width_offsets,
+                    height_offsets + new_heights,
+                    width_offsets + new_widths,
+                ],
+                axis=1,
+            )
+            images = tf.image.crop_and_resize(
+                images, bounding_boxes, tf.range(batch_size), (height, width)
+            )
+            return images
+
 
 def filter_ds_on_indices(ds, indices):
     """Filter a tensorflow dataset on a list of indices
