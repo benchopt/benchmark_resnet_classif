c3befbdb223153fd6d1f9ce192b38bee64a669ab
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2022-10-11 15:31:01 +0200

    added legends in the main fig as it is varying

--- plot_hp_grid_search.py
+++ plot_hp_grid_search.py
@@ -38,7 +38,7 @@ def _remove_prefix(text, prefix):
 def fill_between_x(ax, x, q_min, q_max, y, color, marker, label,
                    plotly=False, alpha=1.0, linestyle='-'):
     if not plotly:
-        ax.plot(
+        handles = ax.plot(
             x,
             y,
             color=color,
@@ -50,7 +50,7 @@ def fill_between_x(ax, x, q_min, q_max, y, color, marker, label,
             linestyle=linestyle,
             markevery=20,
         )
-        # ax.fill_betweenx(y, q_min, q_max, color=color, alpha=.3)
+        return handles
 
 
 def plot_objective_curve(
@@ -82,9 +82,6 @@ def plot_objective_curve(
     markers = {i: v for i, v in enumerate(plt.Line2D.markers)}
 
     df = df.copy()
-    dataset_name = df['data_name'].unique()[0]
-    objective_name = df['objective_name'].unique()[0]
-    title = f"{objective_name}\nData: {dataset_name}" if title is None else title
     df.query(f"`{obj_col}` not in [inf, -inf]", inplace=True)
     if solvers is not None:
         query = ''
@@ -111,6 +108,8 @@ def plot_objective_curve(
         ax.text(0.5, 0.5, "Not Available")
         return ax
 
+    handles = []
+    labels = []
     for i, solver_name in enumerate(solver_names):
         if solvers:
             solver_dict = [
@@ -137,18 +136,16 @@ def plot_objective_curve(
         q1 = df_.groupby('stop_val')['time'].quantile(.1)
         q9 = df_.groupby('stop_val')['time'].quantile(.9)
 
-        fill_between_x(
+        handles.extend(fill_between_x(
             ax, curve['time'], q1, q9, curve[obj_col], color=color,
             marker=marker, label=label, plotly=False, alpha=alpha,
             linestyle=linestyle,
-        )
-    # ax.legend(fontsize=14, loc='upper right')
-    # plt.yscale('log')
+        ))
+        labels.append(label)
     y_lim = [0.04, 0.2] if y_lim is None else y_lim
     if percent:
         y_lim = [y * 100 for y in y_lim]
     ax.set_ylim(y_lim)
-    # ax.set_xlabel("Time (s)", fontsize=labelsize)
     if ylabel is not None:
         if percent:
             ylabel += ' (\%)'
@@ -156,11 +153,27 @@ def plot_objective_curve(
             ylabel,
             fontsize=labelsize,
         )
-    ax.set_title(title, fontsize=labelsize)
+    if title is not None:
+        ax.set_title(title, fontsize=labelsize)
     ax.tick_params(axis='both', which='major', labelsize=ticksize)
-    # plt.tight_layout()
 
-    return ax
+    return ax, handles, labels
+
+
+def extract_hp_value_from_solver_name(solver_name, hp_name):
+    solver_attributes = solver_name.split(',')
+    for attribute in solver_attributes:
+        if attribute.startswith(hp_name):
+            hp_value = attribute.split('=')[1]
+            # strip from potential ] at the end
+            hp_value = hp_value.strip(']')
+            # if float use the scientific notation
+            try:
+                hp_value = float(hp_value)
+                hp_value = f'{hp_value:.1e}'
+            except ValueError:
+                pass
+            return hp_value
 
 
 if __name__ == "__main__":
@@ -171,12 +184,12 @@ if __name__ == "__main__":
         'cifar100': 100 - 78.49,
         'svhn': 2.95,  # from AMP, with pre act
     }
-    dataset = 'cifar100'
+    dataset = 'svhn'
     optimizers = ['SGD', 'Adam']
     hyperparameters = ['lr', 'wd']
     hyperparameter_key_per_optimizer = {
         'lr': {'Adam': 'lr', 'SGD': 'lr'},
-        'wd': {'Adam': 'coupled_weight_decay', 'SGD': 'weight_decay'},
+        'wd': {'Adam': 'decoupled_weight_decay', 'SGD': 'weight_decay'},
     }
     hyperparameter_repr = {
         'lr': 'Learning rate',
@@ -186,14 +199,20 @@ if __name__ == "__main__":
         'lr': {'Adam': 0.001, 'SGD': 0.1},
         'wd': {'Adam': 0.02, 'SGD': 0.0005},
     }
-    if dataset == 'cifar':
-        results_file = 'outputs/resnet_grid_neurips_cifar10.csv'
-    elif dataset == 'cifar100':
-        results_file = 'outputs/resnet_grid_neurips.csv'
+    results_file = f'outputs/resnet_grid_neurips_{dataset}.csv'
     df = pd.read_csv(results_file)
     fig, axs = plt.subplots(2, 2, figsize=[11, 1+2*1], constrained_layout=True, sharey=True, sharex=True)
+    fig = plt.figure(figsize=[11, 1+2*1], constrained_layout=True)
+    outer_gs = fig.add_gridspec(2, 2, hspace=0.6)
+    ref_ax = None
     for i_optimizer, optimizer in enumerate(optimizers):
         for i_hyperparameter, hyperparameter in enumerate(hyperparameters):
+            inner_gs = outer_gs[i_optimizer, i_hyperparameter].subgridspec(
+                nrows=2,
+                ncols=2,
+                height_ratios=[0.1, 2],
+                hspace=-0.1,
+            )
             print('='*20)
             print(optimizer, hyperparameter)
             ylim = {
@@ -202,43 +221,62 @@ if __name__ == "__main__":
                 'cifar100': [0.2, 1],
                 'mnist': [0., 0.03],
             }[dataset]
-            ax = axs[i_optimizer, i_hyperparameter]
+            ax = fig.add_subplot(inner_gs[1, :])
             ax.tick_params(axis='both', which='major', labelsize=labelsize)
             constant_hp = 'lr' if hyperparameter == 'wd' else 'wd'
             constant_hp_repr = hyperparameter_key_per_optimizer[constant_hp][optimizer]
             constant_hp_value = default_hp_values_per_optimizer[constant_hp][optimizer]
-            plot_objective_curve(
+            _, handles, labels = plot_objective_curve(
                 df,
                 ax,
                 obj_col='objective_test_err',
                 solver_filters=[optimizer, f'{constant_hp_repr}={constant_hp_value}'],
-                title=f"{optimizer} - {hyperparameter_repr[hyperparameter]}",
+                title=None,
                 ylabel=None,
                 y_lim=ylim,
                 percent=True,
             )
             sota = sota_resnet[dataset]
             ax.axhline(sota, color='k', linestyle='--')
-    plt.subplots_adjust(hspace=0.6)
+            ax_legend = fig.add_subplot(inner_gs[0, :])
+            ax_legend.axis('off')
+            hp_repr = hyperparameter_key_per_optimizer[hyperparameter][optimizer]
+            extracted_labels = [extract_hp_value_from_solver_name(label, hp_repr) for label in labels]
+            sorted_handles, sorted_labels = zip(*sorted(zip(handles, extracted_labels), key=lambda x: x[1]))
+            ax_legend.legend(
+                sorted_handles,
+                sorted_labels,
+                loc='center',
+                ncol=4,
+                handlelength=1.5,
+                handletextpad=.2,
+            )
+            ax_title = fig.add_subplot(outer_gs[i_optimizer, i_hyperparameter])
+            ax_title.axis('off')
+            ax_title.set_title(
+                f"{optimizer} - {hyperparameter_repr[hyperparameter]}",
+                fontsize=labelsize,
+
+            )
     fig.supylabel('Test error (\%)', fontsize=labelsize, x=0.05)
     fig.supxlabel("Time (s)", fontsize=labelsize, y=-0.01)
     plt.savefig(f'{dataset}_hp_sens.pdf', dpi=300)
     plt.savefig(f'{dataset}_hp_sens.svg', dpi=300)
 
-    ax_example = axs[0, 0]  # we take the cifar axis
-    leg_fig, ax2 = plt.subplots(1, 1, figsize=(20, 4))
-    n_col = 3
-    legend = ax2.legend(
-        ax_example.lines, [line.get_label() for line in ax_example.lines], ncol=n_col,
-        loc="upper center")
-    leg_fig.canvas.draw()
-    leg_fig.tight_layout()
-    width = legend.get_window_extent().width
-    height = legend.get_window_extent().height
-    leg_fig.set_size_inches((width / 80,  max(height / 80, 0.5)))
-    plt.axis('off')
-    fig2_name = f"{dataset}_hp_sens_legend.pdf"
-    leg_fig.savefig(fig2_name, dpi=300)
-    os.system(f"pdfcrop {fig2_name} {fig2_name}")
-    leg_fig.savefig(f"{dataset}_hp_sens_legend.svg", dpi=300)
+    # ax_example = axs[0, 0]  # we take the cifar axis
+    # leg_fig, ax2 = plt.subplots(1, 1, figsize=(20, 4))
+    # n_col = 3
+    # legend = ax2.legend(
+    #     ax_example.lines, [line.get_label() for line in ax_example.lines], ncol=n_col,
+    #     loc="upper center")
+    # leg_fig.canvas.draw()
+    # leg_fig.tight_layout()
+    # width = legend.get_window_extent().width
+    # height = legend.get_window_extent().height
+    # leg_fig.set_size_inches((width / 80,  max(height / 80, 0.5)))
+    # plt.axis('off')
+    # fig2_name = f"{dataset}_hp_sens_legend.pdf"
+    # leg_fig.savefig(fig2_name, dpi=300)
+    # os.system(f"pdfcrop {fig2_name} {fig2_name}")
+    # leg_fig.savefig(f"{dataset}_hp_sens_legend.svg", dpi=300)
 
